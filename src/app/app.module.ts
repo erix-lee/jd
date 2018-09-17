cd5676f2f2824128f6eba444c7dbcda4ede33910
16262824 <16262824@qq.com>
2018-09-18 06:12:03 +0800

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,93 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, Title }  from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+
+import { MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatTabsModule,
+  MatToolbarModule, MatGridListModule, MatTooltipModule } from '@angular/material';
+import { CovalentCommonModule } from '@covalent/core/common';
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentMediaModule } from '@covalent/core/media';
+import { CovalentLoadingModule } from '@covalent/core/loading';
+import { CovalentNotificationsModule } from '@covalent/core/notifications';
+import {  CovalentMenuModule } from '@covalent/core';
+import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RequestInterceptor } from '../config/interceptors/request.interceptor';
+import { MOCK_API } from '../config/api.config';
+
+const httpInterceptorProviders: Type<any>[] = [
+  RequestInterceptor,
+];
+
+export function getAPI(): string {
+  return MOCK_API;
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    AppRoutingModule,
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    HttpModule,
+    // material modules
+
+
+    MatIconModule,
+
+    MatDividerModule,
+    MatInputModule,
+
+    MatButtonModule,
+    MatListModule,
+
+    MatCardModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatTooltipModule,
+    // covalent modules
+    CovalentCommonModule,
+    CovalentLayoutModule,
+    CovalentMediaModule,
+    CovalentLoadingModule,
+    CovalentNotificationsModule,
+    CovalentMenuModule,
+    CovalentHttpModule.forRoot({
+      interceptors: [{
+        interceptor: RequestInterceptor, paths: ['**'],
+      }],
+    }),
+    // external modules
+    NgxChartsModule,
+    // routes
+  
+  ], // modules needed to run this module
+  providers: [
+    httpInterceptorProviders,
+    Title,
+  ], // additional providers needed for this module
+  entryComponents: [ ],
+  bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+import { TdLoadingService } from '@covalent/core/loading';
+
+@Component({
+  selector: 'qs-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+
+  username: string;
+  password: string;
+
+  constructor(private _router: Router,
+    private _loadingService: TdLoadingService) { }
+
+  login(): void {
+    this._loadingService.register();
+    let role = { name: "guest", display: "访客", home: "/" };
+
+    if (this.username.startsWith("1")) {
+      role.name = "admin";
+      role.display = "管理员";
+
+    } else if (this.username.startsWith("2")) {
+      role.name = "director";
+      role.display = "科研室主任";
+
+
+    } else if (this.username.startsWith("3")) {
+      role.name = "chief";
+      role.display = "课程组组长";
+
+    } else if (this.username.startsWith("4")) {
+      role.name = "teacher";
+      role.display = "课程组教师";
+
+
+    } else if (this.username.startsWith("5")) {
+      role.name = "student";
+      role.display = "学生";
+     
+    };
+
+    setTimeout(() => {
+      alert('模拟延迟1秒登录：账号= ' + this.username + "，角色=" + role.name + "\/" + role.display);
+      this._router.navigate([role.name]);
+      this._loadingService.resolve();
+    }, 1000);
+  }
+}
